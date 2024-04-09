6e255398c1db43eee56808ba6eb7cc943e92cacf
didichulam <140446190+didichulam@users.noreply.github.com>
2024-04-09 13:34:34 +0100

    added basic search engine

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
+function searchCity(event) {
+  event.preventDefault();
+  let searchInputSpace = document.querySelector("#search-input-space");
+  let displayedCity = document.querySelector("#displayed-city");
+  displayedCity.innerHTML = searchInputSpace.value;
+}
 
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", searchCity);
